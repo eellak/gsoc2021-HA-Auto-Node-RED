55214eafb02a3314f3e6e0be96046d5155f381bc
CedArctic <kdoumani98@gmail.com>
2021-06-10 12:03:10 +0300

    Added Entity class docstrings

--- lib/entity.py
+++ lib/entity.py
@@ -1,6 +1,7 @@
 from commlib.endpoints import endpoint_factory, EndpointType, TransportType
 from commlib.transports.mqtt import ConnectionParameters, Credentials
 
+
 # An index of all current Entities
 class EntityIndex:
     # Array with references to all entities
@@ -13,7 +14,43 @@ class EntityIndex:
 
 # A class representing an entity communicating via an MQTT broker on a specific topic
 class Entity:
+    """
+    The Entity class represents an entity communicating via an MQTT broker on a specific topic.
+    ...
+
+    Attributes
+    ----------
+        name: str
+            Entity name. e.g: 'temperature_sensor'
+        topic: str
+            MQTT topic on which entity communicates. e.g: 'sensors.temp_sensor' correspons to topic sensors/temp_sensor
+        host: str
+            IP address of the MQTT broker used for communications. e.g: '192.168.1.2'
+        username: str
+            Username used for MQTT broker authentication
+        password: str
+            Password used for MQTT broker authentication
+        state: dictionary
+            Dictionary from the entity's state JSON. Initial state is a blank dictionary {}
+        old_states: list
+            List of dictionaries containing past states
+        automations: list
+            List of references to automations of which the conditions refer to this entity
+        credentials: Credentials object from commlib-py
+        conn_params: ConnectionParameters object from commlib-py
+        mqtt_sub: MQTT subscriber object from commlib-py
+
+
+    """
     def __init__(self, name, topic, host, username, password):
+        """
+        Creates and returns an Entity object
+        :param name: Entity name. e.g: 'temperature_sensor'
+        :param topic: MQTT topic on which entity communicates. e.g: 'sensors.temp_sensor' correspons to topic sensors/temp_sensor
+        :param host: IP address of the MQTT broker used for communications. e.g: '192.168.1.2'
+        :param username: Username used for MQTT broker authentication
+        :param password: Password used for MQTT broker authentication
+        """
         # Entity name
         self.name = name
 
@@ -45,10 +82,21 @@ class Entity:
 
     # Adds an Automation reference to this Entity
     def add_automation(self, automation):
+        """
+        Adds an Automation reference to this Entity. Meant to be called by the Automation constructor
+        :param automation: Reference to the Automation object
+        :return:
+        """
         self.automations.append(automation)
 
     # Callback function for updating Entity state and triggering automations evaluation
     def update_state(self, new_state):
+        """
+        Function for updating Entity state and triggering automations evaluation. Meant to be used as a callback
+        function by the Entity's mqtt_sub MQTT subscriber object (commlib-py).
+        :param new_state: Dictionary containing the Entity's state
+        :return:
+        """
         # Append old state to old states collection
         self.old_states.append(self.state)
 
