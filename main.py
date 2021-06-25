114c3070034c40aefee3415cb5ea05b02358dccd
CedArctic <kdoumani98@gmail.com>
2021-06-25 11:15:08 +0300

    main.py: Discarded previous rapid prototype and began work on new paradigm

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from textx import metamodel_from_file
 
 from lib.automation import Automation, AutomationIndex
 from lib.broker import Broker, broker_index
-from lib.entity import Entity, entity_index
-import config.config as cfg
+from lib.entity import Entity, Attribute, IntAttribute, FloatAttribute, StringAttribute, BoolAttribute
+from lib.build_expression import build_condition
 
 if __name__ == '__main__':
 
@@ -13,70 +13,18 @@ if __name__ == '__main__':
 
     # === Initialize Brokers ====
 
-    # Initialize broker metamodel
-    brokers_meta_tx = metamodel_from_file('lang/broker.tx', classes=[Broker])
+    # Initialize full metamodel
+    metamodel = metamodel_from_file('lang/full_metamodel.tx', classes=[Entity, Attribute, IntAttribute, FloatAttribute,
+                                                                       StringAttribute, BoolAttribute])
 
-    # Read Brokers from files
-    brokers_files = list(config_dir.glob('*.broker'))
-    brokers_files.remove(pathlib.Path("config\\broker_example.broker"))
+    # Initialize full model
+    model = metamodel.model_from_file('config/example.full_metamodel')
 
-    # Iterate through broker files and brokers. Brokers are added to the broker_index through the Broker __init()__.
-    # debug_broker_index will end up being a copy of broker_index from lib.broker
-    debug_broker_index = {}
-    for file in brokers_files:
-        for broker in brokers_meta_tx.model_from_file(file).brokers:
-            debug_broker_index[broker.name] = broker
+    # Build conditions
+    build_condition(model.automations[0].condition, metamodel)
+    print(model.automations[0].condition.cond_lambda)
 
-    # === Initialize Entities ===
-
-    # Initialize entity metamodel
-    entities_meta_tx = metamodel_from_file('lang/entity.tx', classes=[Entity])
-
-    # Read Entities from files
-    entities_files = list(config_dir.glob('*.entity'))
-    entities_files.remove(pathlib.Path("config\\entity_example.entity"))
-
-    # Iterate through entity files and entities. Entities are added to the entity_index through the Entity __init()__.
-    # debug_entity_index will end up being a copy of entity_index from lib.entity
-    debug_entity_index = {}
-    for file in entities_files:
-        for entity in entities_meta_tx.model_from_file(file).entities:
-            debug_broker_index[entity.name] = entity
-
-    # Get sensor Entities from the entity_index
-    temp_sensor = entity_index['temperature_sensor']
-    status_sensor = entity_index['status_sensor']
-
-    # === Initialize Automations ===
-
-    # Create Automation actions and conditions using closures
-    def automation_action_generator(sensor):
-        counter = 0
-
-        def automation_action():
-            nonlocal counter
-            sensor.update_state({'state': f'altered{counter}'})
-            counter = counter + 1
-
-        return automation_action
-
-
-    def automation_condition_generator(sensor):
-
-        def automation_condition():
-            return sensor.state['temperature'] > 10
-
-        return automation_condition
-
-
-    # Create Automation
-    automation = Automation(name='automation',
-                            enabled=True,
-                            condition=automation_condition_generator(temp_sensor),
-                            condition_entities=[temp_sensor],
-                            action=automation_action_generator(status_sensor))
-
-    """
+"""
         To test that everything works as intended just push an MQTT message to the mqtt_sensors/temperature_sensor topic
         with a temperature value > 10 and see if status_sensor's status changes to altered.
         
