32cfff5935e158c19c731ba0a02ea5f8d1633b39
CedArctic <kdoumani98@gmail.com>
2021-06-25 17:26:31 +0300

    main.py: - Updated main.py to incorporate latest changes. - Added entities dictionary to model. Needed for evaluating automation conditions.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pathlib
 from textx import metamodel_from_file
 
 from lib.automation import Automation, AutomationIndex
-from lib.broker import Broker, broker_index
+from lib.broker import Broker, MQTTBroker, AMQPBroker, RedisBroker, BrokerAuthPlain
 from lib.entity import Entity, Attribute, IntAttribute, FloatAttribute, StringAttribute, BoolAttribute
 from lib.build_expression import build_condition
 
@@ -15,14 +15,23 @@ if __name__ == '__main__':
 
     # Initialize full metamodel
     metamodel = metamodel_from_file('lang/full_metamodel.tx', classes=[Entity, Attribute, IntAttribute, FloatAttribute,
-                                                                       StringAttribute, BoolAttribute])
+                                                                       StringAttribute, BoolAttribute, Broker,
+                                                                       MQTTBroker, AMQPBroker, RedisBroker,
+                                                                       BrokerAuthPlain])
 
     # Initialize full model
     model = metamodel.model_from_file('config/example.full_metamodel')
 
-    # Build conditions
+    # Build entities dictionary in model. Needed for evaluating conditions
+    model.entities_dict = {entity.name: entity for entity in model.entities}
+
+    # Build conditions and print them
     build_condition(model.automations[0].condition, metamodel)
     print(model.automations[0].condition.cond_lambda)
+
+    # At this point send the proper statuses to the entities involved in the condition and then evaluate
+    # the Automation's Condition
+    # print(eval(model.automations[0].condition.cond_lambda))
 
 """
         To test that everything works as intended just push an MQTT message to the mqtt_sensors/temperature_sensor topic
