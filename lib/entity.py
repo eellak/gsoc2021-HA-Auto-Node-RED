769d8561c020c25070ecbf3cefa36e853026a411
CedArctic <kdoumani98@gmail.com>
2021-06-25 16:02:58 +0300

    entity.py: - Removed entity index since all entities are in the parsed textX model - Updated Entity to work with new textX metamodel - Added Attribute and related *Attribute classes - Updated docstrings

--- lib/entity.py
+++ lib/entity.py
@@ -1,10 +1,4 @@
 from commlib.endpoints import endpoint_factory, EndpointType, TransportType
-from commlib.transports.mqtt import ConnectionParameters, Credentials
-
-from lib.broker import broker_index
-
-# An index of all current Entities {'entity_name': entity_object}. Gets populated by Entity's __init()__.
-entity_index = {}
 
 
 # A class representing an entity communicating via an MQTT broker on a specific topic
@@ -18,41 +12,32 @@ class Entity:
         name: str
             Entity name. e.g: 'temperature_sensor'
         topic: str
-            MQTT topic on which entity communicates. e.g: 'sensors.temp_sensor' correspons to topic sensors/temp_sensor
-        host: str
-            IP address of the MQTT broker used for communications. e.g: '192.168.1.2'
-        username: str
-            Username used for MQTT broker authentication
-        password: str
-            Password used for MQTT broker authentication
+            Topic on which entity communicates. e.g: 'sensors.temp_sensor' corresponds to topic sensors/temp_sensor
         state: dictionary
             Dictionary from the entity's state JSON. Initial state is a blank dictionary {}
-        old_states: list
-            List of dictionaries containing past states
-        automations: list
-            List of references to automations of which the conditions refer to this entity
-        credentials: Credentials object from commlib-py
-        conn_params: ConnectionParameters object from commlib-py
-        mqtt_sub: MQTT subscriber object from commlib-py
+        subscriber:
+            Communication endpoint built using commlib-py used to subscribe to the Entity's topic
 
     Methods
     -------
         add_automation(self, automation): Adds an Automation reference to this Entity. Meant to be called by the
             Automation constructor
-        update_state(self, new_state): Function for updating Entity state and triggering automations evaluation. Meant
-            to be used as a callback function by the Entity's mqtt_sub MQTT subscriber object (commlib-py).
+        update_state(self, new_state): Function for updating Entity state. Meant to be used as a callback function by
+            the Entity's subscriber object (commlib-py).
 
 
 
     """
-    def __init__(self, parent, name, topic, broker_name):
+
+    def __init__(self, parent, name, topic, broker, attributes):
         """
         Creates and returns an Entity object
         :param name: Entity name. e.g: 'temperature_sensor'
-        :param topic: MQTT topic on which entity communicates. e.g: 'sensors.temp_sensor' corresponds to topic
-            sensors/temp_sensor
-        :param broker_name: Name of the MQTT broker used for communications.
+        :param topic: Topic on which entity communicates using the Broker. e.g: 'sensors.temp_sensor' corresponds to
+                        topic sensors/temp_sensor
+        :param broker: Reference to the Broker used for communications
         :param parent: Parameter required for Custom Class compatibility in textX
+        :param attributes: List of Attribute objects belonging to the Entity
         """
         # TextX parent attribute. Required to use Entity as a custom class during metamodel instantiation
         self.parent = parent
@@ -65,50 +50,56 @@ class Entity:
 
         # Entity state
         self.state = {}
-        self.old_states = []
-
-        # Automations with conditions that involve this entity
-        self.automations = []
-
-        # Add a reference of this Entity in the entity_index
-        if self.name not in entity_index:
-            entity_index[self.name] = self
 
         # Set Entity's MQTT Broker
-        self.broker_name = broker_name
-        self.broker = broker_index[broker_name]
+        self.broker = broker
+
+        # Entity's Attributes
+        self.attributes = attributes
 
         # Create and start MQTT subscriber
-        self.mqtt_sub = endpoint_factory(EndpointType.Subscriber, TransportType.MQTT)(
+        self.subscriber = endpoint_factory(EndpointType.Subscriber, TransportType.MQTT)(
             topic=topic,
             conn_params=self.broker.conn_params,
             on_message=self.update_state
         )
-        self.mqtt_sub.run()
-
-    # Adds an Automation reference to this Entity
-    def add_automation(self, automation):
-        """
-        Adds an Automation reference to this Entity. Meant to be called by the Automation constructor
-        :param automation: Reference to the Automation object
-        :return:
-        """
-        self.automations.append(automation)
+        self.subscriber.run()
 
     # Callback function for updating Entity state and triggering automations evaluation
     def update_state(self, new_state):
         """
-        Function for updating Entity state and triggering automations evaluation. Meant to be used as a callback
-        function by the Entity's mqtt_sub MQTT subscriber object (commlib-py).
+        Function for updating Entity state. Meant to be used as a callback function by the Entity's subscriber object
+        (commlib-py).
         :param new_state: Dictionary containing the Entity's state
         :return:
         """
-        # Append old state to old states collection
-        self.old_states.append(self.state)
 
         # Update state
         self.state = new_state
 
-        # Iterate through automations and evaluate them
-        for automation in self.automations:
-            automation.evaluate()
+
+class Attribute:
+    def __init__(self, parent, name):
+        self.parent = parent
+        self.name = name
+        self.value = None
+
+
+class IntAttribute(Attribute):
+    def __init__(self, parent, name):
+        super().__init__(parent, name)
+
+
+class FloatAttribute(Attribute):
+    def __init__(self, parent, name):
+        super().__init__(parent, name)
+
+
+class StringAttribute(Attribute):
+    def __init__(self, parent, name):
+        super().__init__(parent, name)
+
+
+class BoolAttribute(Attribute):
+    def __init__(self, parent, name):
+        super().__init__(parent, name)
