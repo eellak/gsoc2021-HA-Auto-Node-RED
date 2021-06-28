227a8e6f24f8dd0123b08fc3b55389bea8a2e7f3
CedArctic <kdoumani98@gmail.com>
2021-06-28 13:32:36 +0300

    main.py: - Updated to work with latest textX metamodel - Added seed() which initializes the state of Entities for testing the automation condition - Incorporated build_condition to build Automation conditions - Added a basic evaluation loop which iterates through automations and evaluates them running their actions if evaluation is successful

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pathlib
+import time
+
 from textx import metamodel_from_file
 
-from lib.automation import Automation, AutomationIndex
+from lib.automation import Automation, Action, IntAction, FloatAction, StringAction, BoolAction
 from lib.broker import Broker, MQTTBroker, AMQPBroker, RedisBroker, BrokerAuthPlain
 from lib.entity import Entity, Attribute, IntAttribute, FloatAttribute, StringAttribute, BoolAttribute
 from lib.build_expression import build_condition
+
+
+# Used to initialize entities with data for condition testing purposes
+def seed(entities_dict):
+    # Set temperature
+    entities_dict['temperature_sensor'].publisher.publish({"temperature": 8})
+
+    # Set status
+    entities_dict['status_sensor_2'].publisher.publish({"status": 'At Home'})
+
+    # Set thermostat status
+    entities_dict['thermostat'].publisher.publish({"on": False})
+
 
 if __name__ == '__main__':
 
@@ -17,7 +32,8 @@ if __name__ == '__main__':
     metamodel = metamodel_from_file('lang/full_metamodel.tx', classes=[Entity, Attribute, IntAttribute, FloatAttribute,
                                                                        StringAttribute, BoolAttribute, Broker,
                                                                        MQTTBroker, AMQPBroker, RedisBroker,
-                                                                       BrokerAuthPlain])
+                                                                       BrokerAuthPlain, Automation, Action, IntAction,
+                                                                       FloatAction, StringAction, BoolAction])
 
     # Initialize full model
     model = metamodel.model_from_file('config/example.full_metamodel')
@@ -25,30 +41,24 @@ if __name__ == '__main__':
     # Build entities dictionary in model. Needed for evaluating conditions
     model.entities_dict = {entity.name: entity for entity in model.entities}
 
-    # Build conditions and print them
-    build_condition(model.automations[0].condition, metamodel)
-    print(model.automations[0].condition.cond_lambda)
+    # Build Conditions for all Automations
+    for automation in model.automations:
+        build_condition(automation.condition, metamodel)
+        print(f"{automation.name} condition:\n{model.automations[0].condition.cond_lambda}\n")
 
-    # At this point send the proper statuses to the entities involved in the condition and then evaluate
-    # the Automation's Condition
-    # print(eval(model.automations[0].condition.cond_lambda))
+    # Seed the entities with initial data so the Automation will evaluate to True
+    seed(model.entities_dict)
 
-"""
-        To test that everything works as intended just push an MQTT message to the mqtt_sensors/temperature_sensor topic
-        with a temperature value > 10 and see if status_sensor's status changes to altered.
-        
-        Example message:
-        {
-          "header": {
-            "timestamp": 1623231268109,
-            "properties": {
-              "content_type": "application/json",
-              "content_encoding": "utf8"
-            }
-          },
-          "data": {
-            "temperature": 13
-          }
-        }
-        
-    """
+    # Evaluation loop
+    counter = 1
+    while counter < 24:
+        # Every 4 iterations use seed to set the Automation Entities states so that the Action triggers
+        if counter % 4 == 0:
+            seed(model.entities_dict)
+        # Evaluate automations and print results
+        for automation in model.automations:
+            print(automation.evaluate())
+        # Increase counter
+        counter += 1
+        # Sleep
+        time.sleep(2)
