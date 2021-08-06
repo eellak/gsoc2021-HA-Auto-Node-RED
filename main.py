d101ec0d04ebebd85c579e3e34aa7dd4c6170713
CedArctic <kdoumani98@gmail.com>
2021-08-06 13:00:18 +0300

    main.py: Overhaul - Main now either uses config/config_remote.model, received via MQTT, or the local config/config_local.model as the HA-Auto configuration model depending on the RUN_MODE flag. - Added rn_start_scheduler() which is a callback for a subscriber that receives the configuration model via MQTT. The callback saves the model to config/config_remote.model. - When running with RUN_MODE!="Local", main creates and starts a subscriber that connects to the broker used to receive the configuration model. Configuration for the connection is placed in config/config.py. - Evaluation loop runs forever printing which automations are triggered and which are not. Loop sleeps for 1s.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 import time
+import logging
+from colorama import init, Fore, Style
 
 from textx import metamodel_from_file
+
+from commlib.endpoints import endpoint_factory, EndpointType, TransportType
+from commlib.transports.mqtt import ConnectionParameters as MQTT_ConnectionParameters, Credentials as MQTT_Credentials
 
 from lib.automation import Automation, List, Dict, Action, IntAction, FloatAction, StringAction, BoolAction
 from lib.broker import Broker, MQTTBroker, AMQPBroker, RedisBroker, BrokerAuthPlain
@@ -8,21 +14,25 @@ from lib.entity import Entity, Attribute, \
     IntAttribute, FloatAttribute, StringAttribute, BoolAttribute, ListAttribute, DictAttribute
 
 
-# Used to initialize entities with data for condition testing purposes
-def seed(entities_dict):
-    # Set temperature
-    entities_dict['temperature_sensor'].publisher.publish({"temperature": 8})
+# === Node-RED integration settings ===
+# A Run Mode flag. Set to "MQTT" for the Node-RED integration, or to "Local" for a local config
+RUN_MODE = "MQTT"
 
-    # Set status
-    entities_dict['status_sensor_2'].publisher.publish({"status": 'At Home'})
+# Import the configuration for the broker used to receive the HA-Auto configuration model
+if RUN_MODE != "Local":
+    from config.config import nr_mqtt as nr
 
-    # Set thermostat status
-    entities_dict['thermostat'].publisher.publish({"on": False})
+
+# Function used as a callback by the commlib subscriber when receiving the HA-Auto configuration from a broker.
+# It extracts the configuration and writes it to a file.
+def rn_start_scheduler(data):
+    logging.info("Configuration Received")
+    with open(f"config/config_remote.model", 'w') as f:
+        f.write(data["config"])
+        f.close()
 
 
 if __name__ == '__main__':
-
-    # === Initialize Brokers ====
 
     # Initialize full metamodel
     metamodel = metamodel_from_file('lang/full_metamodel.tx', classes=[Entity, Attribute, IntAttribute, FloatAttribute,
@@ -32,8 +42,34 @@ if __name__ == '__main__':
                                                                        IntAction, FloatAction, StringAction, BoolAction,
                                                                        List, Dict])
 
-    # Initialize full model
-    model = metamodel.model_from_file('config/example.full_metamodel')
+    # Determine the configuration file path: remote or local
+    if RUN_MODE == "MQTT":
+        # Create and run subscriber to connect to MQTT broker to receive the model sent by the Node-RED integration
+        nr_credentials = MQTT_Credentials(nr["username"], nr["password"])
+        nr_conn_params = MQTT_ConnectionParameters(host=nr["host"], port=nr["port"], creds=nr_credentials)
+        subscriber = endpoint_factory(EndpointType.Subscriber, TransportType.MQTT)(
+            topic=nr["topic"],
+            conn_params=nr_conn_params,
+            on_message=rn_start_scheduler
+        )
+        subscriber.run()
+
+        # Wait to receive the model
+        while not os.path.exists("config/config_remote.model"):
+            time.sleep(1)
+
+        # Stop the subscriber
+        subscriber.stop()
+
+        # The path where the subscriber callback saved the received model
+        model_path = "config/config_remote.model"
+
+    else:
+        # Define the default local configuration path
+        model_path = "config/config_local.model"
+
+    # Parse model
+    model = metamodel.model_from_file(model_path)
 
     # Build entities dictionary in model. Needed for evaluating conditions
     model.entities_dict = {entity.name: entity for entity in model.entities}
@@ -43,19 +79,19 @@ if __name__ == '__main__':
         automation.build_condition()
         print(f"{automation.name} condition:\n{automation.condition.cond_lambda}\n")
 
-    # Seed the entities with initial data so the Automation will evaluate to True
-    seed(model.entities_dict)
-
     # Evaluation loop
-    counter = 1
-    while counter < 24:
-        # Every 4 iterations use seed to set the Automation Entities states so that the Action triggers
-        if counter % 4 == 0:
-            seed(model.entities_dict)
-        # Evaluate automations and print results
+    while True:
+        # Evaluate automations, run applicable actions and print results
         for automation in model.automations:
-            print(automation.evaluate())
-        # Increase counter
-        counter += 1
+            # Evaluate
+            triggered, msg = automation.evaluate()
+            # Check if action is triggered
+            if triggered:
+                print(f"{Fore.MAGENTA}{automation.name}: {triggered}{Style.RESET_ALL}")
+                # If automation triggered run its actions
+                automation.trigger()
+            else:
+                print(f"{automation.name}: {triggered}")
+
         # Sleep
-        time.sleep(2)
+        time.sleep(1)
